d3cb3ba3b1254f842f809c0ea81fd8378fb34066
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2015-04-24 16:58:23 -0500

    UI related methods

--- src/DynLB.thrift
+++ src/DynLB.thrift
@@ -5,6 +5,15 @@ namespace cpp Comm
 */
 // This is also a valid comment
 
+struct UIState
+{
+	1:double cpu_util;
+	2:i32    nJobsPending;
+	3:i32    nJobsCompleted;
+	4:double fThrottling;
+	5:double dNetwork;
+}
+
 service DynLBServer
 {
 	oneway void SendJobsToRemote(1:i32 size, 2:list<binary> vJobs);
@@ -15,4 +24,7 @@ service DynLBServer
 	oneway void RequestCompletedJobsFromRemote();
 	// we will need synchronous calls during GUI impl to be called 
 	// from java UI since server has no proxy to the UI client.
+	UIState GetStateInfo();
+	void    SetThrottling();
+	
 }
